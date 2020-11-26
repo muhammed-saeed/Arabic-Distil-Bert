adc3fb3af1036ac3c80943e675c26938dc667006
Muhammed Saeed <38116007+muhammed-saeed@users.noreply.github.com>
2020-11-26 17:10:17 +0200

    Add files via upload

---
+++ token_automate.py
@@ -0,0 +1,11 @@
+import os
+
+for i in range(27):
+    argss = 'python3 create_pretraining_data.py --input_file=gs://sudabert_bucket/sudanese_txt_cleaned_shuffled_splits/suda_txt_'+ str(i) +'.txt --output_file=gs://sudabert_bucket/tmp3/tf_examples.tfrecord_' + str(i) + ' --vocab_file=gs://sudabert_bucket/ar_bert_large/vocab.txt --do_lower_case=True --max_seq_length=128 --max_predictions_per_seq=20 --masked_lm_prob=0.15 --random_seed=12345 --dupe_factor=5'
+
+
+    
+    #input_file1 = 'gs://sudabert_bucket/sudanese_txt_cleaned_shuffled_splits/suda_txt_' + str(i) +'.txt '
+    #output_file1 = 'output_file=gs://sudabert_bucket/tmp3/tf_examples.tfrecord_'+ str(i)
+
+    os.system(argss)
