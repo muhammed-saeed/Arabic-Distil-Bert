402892984206fffb479776014b012f49528a3606
muhammed-saeed <mohammed.yahia3@gmail.com>
2021-10-06 14:07:38 +0200

    another file for the sampling

---
+++ huge_text_to_smaller_mukhos_method.py
@@ -0,0 +1,14 @@
+lines_per_file = 55000
+smallfile = None
+num_lines = sum(1 for line in open('ar.txt'))
+with open('ar.txt') as bigfile:
+    for lineno, line in enumerate(bigfile):
+        if lineno % lines_per_file == 0:
+            if smallfile:
+                smallfile.close()
+            small_filename = 'araDistilBertVocabFile2/small_file_{}.txt'.format(lineno + lines_per_file)
+            smallfile = open(small_filename, "w")
+            print(str(lineno) + '/' + num_lines)
+        smallfile.write(line)
+    if smallfile:
+        smallfile.close()
