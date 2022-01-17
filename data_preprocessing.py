be2d5d80ea59aa7b44631ac9c194342a6af44847
Ahmed-5 <ahmedelhassen11@gmail.com>
2022-01-17 06:53:48 +0200

    preprocessing .json.gz files

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,11 +3,11 @@ from cgitb import text
 import json
 import io
 import os
+import gzip
 
-
-files = list(filter(lambda x: x.endswith(".json"), os.listdir()))
+files = list(filter(lambda x: x.endswith(".json.gz"), os.listdir()))
 # list all the files those ending with json
-print(files)
+# print(files)
 
 
 def processing(encoded_line):
@@ -20,9 +20,11 @@ def processing(encoded_line):
 def pre_processing_all(my_file):
 
     # text_data = open("c4-ar.txt", encoding="utf-8");
-    text_data = open(my_file, encoding="utf-8")
+    text_data = gzip.open(my_file)
 
     encoded_lines = text_data.readlines()
+
+    # print(encoded_lines)
 
     decoded_lines = list(map(processing, encoded_lines))
     text_file = list(map(lambda x: x["text"], decoded_lines))
