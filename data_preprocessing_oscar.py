68f439fc458758113f98a96e75364dba920dd580
muhammed.saeed <mohammed.yahia3@gmail.com>
2022-01-31 23:57:25 +0200

    adding more lines

--- data_preprocessing_oscar.py
+++ data_preprocessing_oscar.py
@@ -52,3 +52,8 @@ list(map(pre_processing_all, files))
 
 
 # note three days without codeing
+
+# export const fetchPosts = () => axios.get(url);
+# export const createPost = (newPost) => axios.post(url, newPost);
+# export const updatePost = (id, updatedPost) =>
+#   axios.patch(`${url}/${id}`, updatedPost);
