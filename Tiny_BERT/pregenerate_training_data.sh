5c7f58957449c09cca6b8f6be6f883b7a3dde2cf
LST <mohammed.yahia3@gmail.com>
2022-08-18 00:19:58 +0200

    generate distil

--- Tiny_BERT/pregenerate_training_data.sh
+++ Tiny_BERT/pregenerate_training_data.sh
@@ -1,7 +1,8 @@
 python3 /home/CE/musaeed/Pretrained-Language-Model/TinyBERT/pregenerate_training_data.py \
-  --train_corpus /home/CE/musaeed/ar/xaa \
+  --train_corpus /home/CE/musaeed/ar/ar_part1-36.txt \
+  --reduce_memory \
   --bert_model /home/CE/musaeed/Tiny_BERT/BERT_FILE \
   --do_lower_case \
+  --num_workers 1 \
   --epochs_to_generate 3 \
-  --num_workers 1 --reduce_memory \
-  --output_dir /home/CE/musaeed/train_data_tiny_bert
+  --output_dir /home/CE/musaeed/train_data_tiny_bert_ar
